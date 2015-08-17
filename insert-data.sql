ae81125df9bdf04920b724d955b3dac3e99b14a9
liyuanyuan57 <liyuanyuan0507@outlook.com>
2015-08-17 16:59:23 +0800

    insert Class

--- insert-data.sql
+++ insert-data.sql
@@ -4,10 +4,38 @@ begin
 declare num int;
 set num=1;
 while num <= 20 do
-INSERT INTO  Course(course_name) values(concat("course", num));
+INSERT INTO  Course(course_name) values(concat("course-", num));
 set num=num+1;
 end while;
 end
  //
 
- call course();
+ call course();//
+
+ delimiter //
+ create procedure class()
+ begin
+ declare num int;
+ set num=1;
+ while num <= 5 do
+ INSERT INTO  Class(class_name) values(concat("class-", num));
+ set num=num+1;
+ end while;
+ end
+  //
+
+  call class();//
+
+  delimiter //
+  create procedure Students()
+  begin
+  declare num int;
+  set num=1;
+  while num <= 5 do
+  INSERT INTO  Class(class_name) values(concat("class-", num));
+  set num=num+1;
+  end while;
+  end
+   //
+
+   call class();//
